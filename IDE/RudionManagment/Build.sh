05245a25a52371ee6698036c5c5cb234e3dc55b0
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-11-24 01:33:17 +0300

    add build.sh

--- IDE/RudionManagment/Build.sh
+++ IDE/RudionManagment/Build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cpp_file_paht = $1
-rudion_tools_path = /home/dima/.arduino15/packages/Rudiron/tools
+# need to CmakeList.txt in cpp_file dir
+cpp_file_paht=$1
+rudion_tools_path=/home/dima/.arduino15/packages/Rudiron/tools
+
+touch "$cpp_file_paht"/../libraries.txt
+
+rm -r build
+mkdir build
+
+"$rudion_tools_path"/cmake/default/bin/cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -S "&cpp_file_paht"/.. -B ./build
+
+"$rudion_tools_path"/cmake/default/bin/cmake --build build --target all --
 
