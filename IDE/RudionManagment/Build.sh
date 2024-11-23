4b433b33d90a88859d5648f95485c5a9ca31530a
EzhoffDmitriy <ezhoff.dmitriy@gmail.com>
2024-11-24 02:29:44 +0300

    add upload.sh

--- IDE/RudionManagment/Build.sh
+++ IDE/RudionManagment/Build.sh
@@ -9,7 +9,8 @@ touch "$cpp_file_paht"/../libraries.txt
 rm -r build
 mkdir build
 
-"$rudion_tools_path"/cmake/default/bin/cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -S "&cpp_file_paht"/.. -B ./build
+"$rudion_tools_path"/cmake/default/bin/cmake DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S . -B ./build -G Ninja
 
 "$rudion_tools_path"/cmake/default/bin/cmake --build build --target all --
+
 
--- IDE/RudionManagment/Upload.sh
+++ IDE/RudionManagment/Upload.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cpp_file_paht = $1
-rudion_tools_path = /home/dima/.arduino15/packages/Rudiron/tools
+# needs to build
+cpp_file_paht=$ realpath $1
+fullpath=$2
+rudion_tools_path=/home/dima/.arduino15/packages/Rudiron/tools
+chmod a+rw /dev/ttyUSB0
 
+"$rudion_tools_path"/Rudiron\ Programmer/default/Rudiron\ Programmer "$rudion_tools_path"/bootloaders/default/MDR32F9Qx_default.hex ./build/Sketch.hex --erase --load --run --speed 8
